a5b1850a909b0b3a235563e1c9c528b5338cfd13
Marcio Carolino <marciocarolino@gmail.com>
2021-04-28 22:20:33 -0300

    Comecando o front, já está pegando via console.log o servidor mongoose

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
 import './App.css';
+import api from '../src/service/api'
+import { Component } from 'react';
 
+class App extends Component{
+
+    state = {
+      computadores:[],
+    }
+    async componentDidMount(){
+      const response = await api.get('/computador');
+      console.log(response.data);
+      this.setState({computadores: response.data});
+    }
+
+
+  render(){
+   const {computadores} = this.state;
+
+    return(
+      <div>
+        <h1>Listar os computadores</h1>
+        {computadores.map(computador =>{
+          <li key={computador.active = true}>
+            <h2>
+              <strong>Computador: </strong>
+              {computador.nomeComputador}
+            </h2>
+          </li>
+        })}
+      </div>
+    );
+  }
+}
+
+/*
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        
       </header>
     </div>
   );
 }
-
+*/
 export default App;
